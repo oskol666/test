cbea2f06f3195f333f09598cdd16d1e9826ecc69
oskol666 <oskolmusic@gmail.com>
2020-10-18 17:47:53 +0300

    added unit test

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -2,12 +2,20 @@
 
 namespace TestApp
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+        public static int Min(int a, int b, int c)
         {
-            Console.WriteLine("Hello Oleksii!");
-            Console.ReadKey(false);
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Demo of Unit-testing in C#.");
+            Console.ReadKey();
         }
     }
 }
---
+++ TestApp/TestAppTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MinTest()
+        {
+            Assert.AreEqual(1,Program.Min(1,2,3));
+        }
+
+    }
+}
